91405ca6d9be1cde270f75d4d870ee5f11910052
hammad.hassan <hammadhass72@gmail.com>
2024-11-02 09:35:42 +0500

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -50,9 +50,9 @@ function MainComponent({ onSelect }) {
     <div className='flex flex-col justify-start'>
       <div className='flex flex-col -mt-5 justify-start items-center bg-none border-b-black '>
         <img className='w-3/5' src={Logo} alt='Logo' />
-        <span className='font-bold text-3xl -mt-5'>User Dashboard</span>
+        <span className='font-bold text-3xl -mt-2'>User Dashboard</span>
       </div>
-      <div className='flex h-72 flex-col justify-evenly mt-5'>
+      <div className='flex h-96 flex-col justify-evenly mt-2'>
         <div className='rounded-xl flex mx-auto w-4/5 py-4 font-semibold bg-[#2cc40d] hover:bg-[#17502d] hover:text-white justify-center'
           onClick={() => onSelect('bookSlot')}
         >
@@ -61,12 +61,12 @@ function MainComponent({ onSelect }) {
         <div className='rounded-xl flex mx-auto w-4/5 py-4 font-semibold bg-[#2cc40d] hover:bg-[#17502d] justify-center hover:text-white'
           onClick={() => onSelect('navigateSlot')}
         >
-          Navigate Booked Slot
+          NAVIGATE BOOKED SLOT
         </div>
         <div className='rounded-xl flex mx-auto w-4/5 py-4 bg-[#2cc40d] font-semibold hover:bg-[#17502d] hover:text-white justify-center'
           onClick={() => onSelect('navigateKiet')}
         >
-          Navigate KIET
+          NAVIGATE KIET
         </div>
       </div>
     </div>
