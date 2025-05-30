ecfe345ce79386a117c3c9b4dd8f905e8edc42ad
Hammad Hassan <hammadhass72@gmail.com>
2025-05-31 00:16:09 +0500

    dfgdfdfg

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import NavigateSlot from "./Components/NavigateSlot";
 import BottomBar from "./Components/BottomBar";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { getDeviceSpeed, sendSpeed } from "./getSpeed";
+import { getAverageSpeed, getDeviceSpeed, sendSpeed } from "./getSpeed";
 import GPSAccessPrompt from "./Components/GPSAccessPrompt";
 
 const initialResponse = [
@@ -119,34 +119,34 @@ function App() {
   }, []);
 
   const getUserConsent = async () => {
-    // try {
-    //   const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/User`, {
-    //     method: "POST",
-    //     headers: {
-    //       "CONTENT-TYPE": "application/json",
-    //     },
-    //     body: JSON.stringify({
-    //       consent: true,
-    //     }),
-    //   });
-    //   if (response.ok) {
-    //     const data = await response.json();
-    //     localStorage.setItem("userAgreement", data.userId);
-    //     toast.success("Thanks for coming.", {
-    //       position: "top-right",
-    //     });
-    //   } else {
-    //     toast.error("Server Error. please wait", {
-    //       position: "top-right",
-    //     });
-    //   }
-    // } catch (err) {
-    //   toast.error("Server Error. please wait", {
-    //     position: "top-right",
-    //   });
-    // }
+    try {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/User`, {
+        method: "POST",
+        headers: {
+          "CONTENT-TYPE": "application/json",
+        },
+        body: JSON.stringify({
+          consent: true,
+        }),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("userAgreement", data.userId);
+        toast.success("Thanks for coming.", {
+          position: "top-right",
+        });
+      } else {
+        toast.error("Server Error. please wait", {
+          position: "top-right",
+        });
+      }
+    } catch (err) {
+      toast.error("Server Error. please wait", {
+        position: "top-right",
+      });
+    }
 
-     localStorage.setItem("userAgreement", "85 ");
+    //  localStorage.setItem("userAgreement", "85 ");
   };
 
   const fetchannouncements = async () => {
@@ -311,7 +311,7 @@ function App() {
         setTimeout(() => {
           setIsParked(true);
         }, 10 * 1000);
-        const speed1 = await getDeviceSpeed(3);
+        const speed1 = await getAverageSpeed(3);
         setSpeed(speed1);
         const sResponse = await sendSpeed(speed1, data.reservationId);
         clg(sResponse);
